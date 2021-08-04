574d0cf8e883a7d6fe9b006406b9c952c0018ebd
kexue <xana278@foxmail.com>
2021-08-05 00:06:42 +0800

    init

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nonebot-plugins-heweather",
+    name="nonebot_plugin_heweather",
     version="0.0.1",
     author="kexue",
     author_email="xana278@qq.com",
     description="Get Heweather information and convert to pictures",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kexue-z/nonebot-plugins-heweather",
+    url="https://github.com/kexue-z/nonebot-plugin-heweather",
     project_urls={
-        "Bug Tracker": "https://github.com/kexue-z/nonebot-plugins-heweather/issues",
+        "Bug Tracker": "https://github.com/kexue-z/nonebot-plugin-heweather/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "nonebot-plugins-heweather"},
-    packages=setuptools.find_packages(where="nonebot-plugins-heweather"),
+    # package_dir={"": "nonebot_plugin_heweather"},
+    # packages=setuptools.find_packages(where="nonebot_plugin_heweather"),
     python_requires=">=3.9",
 )
