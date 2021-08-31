d236c3d4949746ee373c0c2cc448061978934c9e
kexue <xana278@foxmail.com>
2021-08-31 17:59:05 +0800

    🔖 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nonebot-plugin-heweather",
-    version="0.0.5",
+    version="0.1.0",
     author="kexue",
     author_email="xana278@qq.com",
     description="Get Heweather information and convert to pictures",
