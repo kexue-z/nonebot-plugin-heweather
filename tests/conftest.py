80fe8ea97fac860b6003af96bb51ec280f05cf94
kexue <x@kexue.io>
2022-08-09 09:28:45 +0800

    ✅ 添加测试

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from nonebug.app import App
+
+from .api import api_key, api_type
+
+
+@pytest.fixture
+async def app(
+    nonebug_init: None,
+    monkeypatch: pytest.MonkeyPatch,
+) -> App:
+    import nonebot
+
+    config = nonebot.get_driver().config
+
+    config.qweather_apikey = api_key
+    config.qweather_apitype =api_type
+
+
+    # 加载插件
+    nonebot.load_plugin("nonebot_plugin_heweather")
+    
+    return App(monkeypatch)
---
+++ tests/test_heweather.py
@@ -0,0 +1,31 @@
+import pytest
+from nonebug import App
+
+from .api import api_key, api_type
+
+
+@pytest.mark.asyncio
+async def test_heweather(app:App, monkeypatch: pytest.MonkeyPatch):
+    from io import BytesIO
+
+    import nonebot
+    from PIL import Image
+    from nonebot import get_driver
+    from nonebot.adapters.onebot.v11 import Bot, Adapter, Message
+    from nonebot_plugin_heweather.render_pic import render
+    from nonebot_plugin_heweather.weather_data import (Weather,
+                                                       CityNotFoundError)
+    driver = get_driver()
+    
+    with monkeypatch.context() as m:
+        async with app.test_server():
+
+            w_data = Weather(city_name='beijing', api_key=api_key, api_type=api_type)
+            try:
+                await w_data.load_data()
+            except CityNotFoundError:
+                pass
+            
+            img = await render(w_data)
+            a = Image.open(BytesIO(img))
+            a.save("weather.png", format="PNG")
