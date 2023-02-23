881617d5779adc3cdb06d0c74c024fda74253374
kexue <x@kexue.io>
2023-02-23 20:15:34 +0800

    ✅ 修改测试

--- tests/test_heweather.py
+++ tests/test_heweather.py
@@ -12,14 +12,14 @@ async def test_heweather(app:App, monkeypatch: pytest.MonkeyPatch):
     from PIL import Image
     from nonebot import get_driver
     from nonebot.adapters.onebot.v11 import Bot, Adapter, Message
+
     from nonebot_plugin_heweather.render_pic import render
     from nonebot_plugin_heweather.weather_data import (Weather,
                                                        CityNotFoundError)
     driver = get_driver()
     
     with monkeypatch.context() as m:
-        async with app.test_server():
-
+        async with (await app).test_server():  # type: ignore
             w_data = Weather(city_name='beijing', api_key=api_key, api_type=api_type)
             try:
                 await w_data.load_data()
