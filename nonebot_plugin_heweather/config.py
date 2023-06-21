6299ad905693ba603a29cde1303be0e1ff863906
kexue <xana278@foxmail.com>
2023-06-21 15:14:02 +0800

    🐛 fix

--- nonebot_plugin_heweather/config.py
+++ nonebot_plugin_heweather/config.py
@@ -5,7 +5,7 @@ from pydantic import Extra, BaseModel
 
 class Config(BaseModel, extra=Extra.ignore):
     qweather_apikey: Optional[str] = None
-    qweather_apitype: Optional[int] = None
+    qweather_apitype: Optional[str] = None
     debug: bool = False
 
 
